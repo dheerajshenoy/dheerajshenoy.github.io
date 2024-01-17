f6ed4c9724c6a90aaa7210c207c6e08e1895b391
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2024-01-17 20:41:11 +0530

    added jquery and todo box animation

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 var windowWidth = window.innerWidth;
 
 function updateTitleOnResize() {
-    
+
     var default_text = "dheerajshenoy.github.io";
     var small_text = "dheerajshenoy";
+    var very_small_text = "djs";
 
     var text_element = document.getElementById("header_title");
     var windowWidth = window.innerWidth;
@@ -11,10 +12,20 @@ function updateTitleOnResize() {
     if (windowWidth >= 600) {
         text_element.innerHTML= default_text;
     }
-    else {
+    else if (windowWidth < 600 && windowWidth > 400) {
         text_element.innerHTML= small_text;
     }
-    
+
+    else if (windowWidth <= 400) {
+        text_element.innerHTML= very_small_text;
+    }
+
+}
+
+// Handle todo button behaviour
+function todo_btn_click() {
+    var todo_box = $("#todo-box");
+    todo_box.toggle("slow");
 }
 
 window.onload = function () {
